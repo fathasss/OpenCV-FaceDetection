f1aca50ca207b0b29d892fdb562b6c8b7c57cf5b
Fatih HAS <32196738+fathasss@users.noreply.github.com>
2021-04-24 17:54:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#%%
+#OpenCV Library import
+import cv2 as opencv
+
+#%%
+#Application
+faceCascade = opencv.CascadeClassifier(
+    r"classifier/haarcascade_eye_tree_eyeglasses.xml")
+#Algılanacak öğeye göre xml dosyasının seçimi.
+
+img = opencv.imread(r"images/IMG_20210409_160525.png")
+#Application for photo import
+
+gray = opencv.cvtColor(img, opencv.COLOR_BGR2RGB)
+#Photo gray mode
+
+faces = faceCascade.detectMultiScale(gray, 1.1,4)
+#Threshold values
+
+for (x,y,w,h) in faces:
+    opencv.rectangle(img,(x,y),(x+w, y+h),(0,255,0),3)
+
+opencv.imshow('photo',img)
+#Show face in photo
+
+opencv.waitKey(0)
+# %%
